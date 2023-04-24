21519778fbfdeb7c523b9a90a0030435b72f8d7f
mastit20 <118136690+Krypt0n112@users.noreply.github.com>
2023-04-24 19:41:19 +0200

    Create Controller.java
    First RequestMapping created
    
    Works

---
+++ DBS-Project/src/main/java/main/Controller.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/")
+	public String greeting() {
+		return "Hello Esslingen SS23";
+	}
+}
